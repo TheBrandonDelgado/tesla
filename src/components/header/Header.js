bb1eb0b7767792ada76be225e440c8957d6e1ae7
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-04-01 14:17:12 -0500

    Added nonfunctional nav menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './../../assets/tesla-logo.png';
 import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleQuestion, faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
@@ -10,6 +13,44 @@ function Header() {
                     <img className="logo" src={logo} alt="Tesla logo"></img>
                 </a>
             </h1>
+            <ol id="menu-items-middle">
+                <li>
+                    <button>
+                        <span>Vehicles</span>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <span>Energy</span>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <span>Charging</span>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <span>Discover</span>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <span>Shop</span>
+                    </button>
+                </li>
+            </ol>
+            <ol id="menu-items-end">
+                <li>
+                    <FontAwesomeIcon icon={faCircleQuestion} style={{ color: '#ffffff'}} size="lg" />
+                </li>
+                <li>
+                    <FontAwesomeIcon icon={faGlobe} style={{ color: '#ffffff'}} size="lg" />
+                </li>
+                <li>
+                    <FontAwesomeIcon icon={faCircleUser} style={{ color: '#ffffff'}} size="lg" />
+                </li>
+            </ol>
         </div>
     );
 }
